c265b721ceae54c679eb94352cbbb04eea942ee9
Luiz Henrique <81529238+IkkyLuiz@users.noreply.github.com>
2022-07-30 16:32:36 -0300

    Add files via upload

---
+++ python/aula2.py
@@ -0,0 +1,9 @@
+a = int(input('Entre com o primeiro valor:'))
+b = int(input('Entre com o segundo valor:'))
+print(type(a))
+soma = a + b
+subtracao = a - b
+multiplicacao = a * b
+divisao = a/b 
+resto = a % b
+print('soma: {}. \nsubtracao: {}. \nmultiplicacao: {}. \ndivisao: {}. \nresto: {}'.format(soma,subtracao,multiplicacao,divisao,resto))
